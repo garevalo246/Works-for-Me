6a3342d7b842b20cc4a3c741ab26a4f72db73aba
garevalo246 <giovanniarevalo246@gmail.com>
2021-04-27 15:53:51 -0700

    My Project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+
+app.use(express.json({ limit: "30mb", extented: true }));
+app.use(express.urlencoded({ limit: "30mb", extented: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://garevalo246:Gqlc8zHSoixfMkkx@cluster0.xybmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
